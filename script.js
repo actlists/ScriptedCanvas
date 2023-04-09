cfa70900024b4e29b8a8f650f0489c22edee3dbe
luxius <47500575+actlists@users.noreply.github.com>
2023-04-09 17:38:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ globalThis.RUN = new class {
 
     async runScript() {
         this.button.title = ""
-        const X = this.canvas.width / 2//this.inputs.x.value
+        const X = this.canvas.width/2//this.inputs.x.value
         const Y = 256//this.inputs.y.value
         const colored = this.inputs.c.checked
         const slow = this.inputs.slow.checked
@@ -29,11 +29,11 @@ globalThis.RUN = new class {
         let getVal = null;
         let done = false;
         switch (this.inputs.m.value) {
-            case 'N': getVal = (x) => (x); break;
-            case 'S': getVal = (x) => (x + 128); break;
-            case 'F': getVal = (x) => (Math.max(-1, Math.min(1, x))) * 127.5 + 128; break;
+            case 'N': getVal = (x)=>(x); break;
+            case 'S': getVal = (x)=>(x+128); break;
+            case 'F': getVal = (x)=>(Math.max(-1,Math.min(1,x)))*127.5+128; break;
 
-            default: getVal = (x) => (NaN); break;
+            default: getVal = (x)=>(NaN); break;
         }
         // Easy math functions
         const params = Object.getOwnPropertyNames(Math);
@@ -51,7 +51,7 @@ globalThis.RUN = new class {
             this.button.title = err.stack
         }
         if (done) {
-            window.location.hash = encodeURIComponent(btoa(JSON.stringify({ code: this.inputs.fV.value, mode: this.inputs.m.value })))
+            window.location.hash = encodeURIComponent(btoa(JSON.stringify({code: this.inputs.fV.value, mode:this.inputs.m.value})))
             try {
                 for (let xP = 0; xP < X * this.inputs.s.value / ZOOM; xP++) {
                     for (let yP = 0; yP < Y; yP++) {
@@ -63,18 +63,18 @@ globalThis.RUN = new class {
 
                         // Convert brightness to grayscale color
                         let color
-
-                        if (Array.isArray(V) && !colored) {
-                            V = [getVal(V[0]), getVal(V[1])]
+                        
+                        if (Array.isArray(V)&&!colored) {
+                            V=[getVal(V[0]),getVal(V[1])]
                             const N1 = isNaN(V[0]) ? 100 : 0;
                             const N2 = isNaN(V[1]) ? 100 : 0;
-                            color = `rgb(${N2 | N1 ? N2 | N1 : V[1] & 255},${N1 ? N1 : V[0] & 255},${N2 ? 0 : V[1] & 255})`
-                        } else if (Array.isArray(V) && colored) {
+                            color = `rgb(${N2|N1?N2|N1:V[1]&255},${N1?N1:V[0]&255},${N2?0:V[1]&255})`
+                        } else if (Array.isArray(V)&&colored) {
                             V = getVal(V[0])
                             const N = isNaN(V) ? 100 : 0;
                             color = `rgb(${N ? 100 : (!colored ? V & 255 : V >> 16 & 255)}, ${N ? 0 : (!colored ? V & 255 : V >> 0 & 255)}, ${N ? 0 : (!colored ? V & 255 : V >> 8 & 255)})`;
                         } else {
-                            V = getVal(V)
+                            V=getVal(V)
                             const N = isNaN(V) ? 100 : 0;
                             color = `rgb(${N ? 100 : (!colored ? V & 255 : V >> 16 & 255)}, ${N ? 0 : (!colored ? V & 255 : V >> 0 & 255)}, ${N ? 0 : (!colored ? V & 255 : V >> 8 & 255)})`;
                         }
@@ -95,7 +95,7 @@ globalThis.RUN = new class {
 
 let hash = window.location.hash
 
-if (hash) {
+if(hash){
     try {
         const data = JSON.parse(atob(decodeURIComponent(hash.slice(1))))
         console.log(data)
@@ -109,7 +109,7 @@ if (hash) {
 window.onresize = RUN.inputs.s.onchange = () => {
     const WIDE = window.innerWidth
     console.log(String(WIDE) + String(WIDE < 1056))
-    if (WIDE < 1056) {
+    if (WIDE < 1056){
         RUN.canvas.width = 512;
     } else {
         RUN.canvas.width = 1024;
